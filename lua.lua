619db325550a30540a31e6ea29c3dba7e0cc5b66
kabsa <yahyaede4@gmail.com>
2025-03-23 11:54:22 -0700

    Create lua.lua

---
+++ lua.lua
@@ -0,0 +1,15 @@
+local function additions(num1, num2)
+	local result = num1 + num2
+	
+	if result == 4  then
+		print("the result is equal to 4")
+	elseif result == 6 then
+		print("the result is equal to 6")
+	else 
+		print("none of these results add up")
+	end
+end
+
+additions(2, 2)
+additions(3, 3)
+additions(1, 1)
